d536908a4d9c4ee90d5f407c3f1f0b5bbb3fd426
Adwaith Ajith <adwaith.shiniajith@edu.sait.ca>
2024-10-17 23:46:01 -0600

    protected sections
    dropdown profile, some sidebar components - only visible for logged-in users.

--- projectVD/src/components/Header/index.tsx
+++ projectVD/src/components/Header/index.tsx
@@ -4,11 +4,28 @@ import DropdownMessage from "./DropdownMessage";
 import DropdownNotification from "./DropdownNotification";
 import DropdownUser from "./DropdownUser";
 import Image from "next/image";
+import { useState } from "react";
 
 const Header = (props: {
   sidebarOpen: string | boolean | undefined;
   setSidebarOpen: (arg0: boolean) => void;
 }) => {
+  const [user, setUser] = useState(null);
+  const userEmail = localStorage.getItem("userEmail");
+
+  const fetchUserData = async (email) => {
+    try {
+      const response = await fetch(`http://localhost:8000/getUser?email=${email}`);
+      if (response.ok) {
+        const userData = await response.json();
+        setUser(userData);
+      } else {
+        console.error("Failed to fetch user data");
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
@@ -118,7 +135,7 @@ const Header = (props: {
           </ul>
 
           {/* <!-- User Area --> */}
-          <DropdownUser />
+          {userEmail && <DropdownUser />}
           {/* <!-- User Area --> */}
         </div>
       </div>
