657d747b782e5c5359530cf8aa051c15af7717e1
Adwaith Ajith <adwaith.shiniajith@edu.sait.ca>
2024-11-01 05:17:26 -0600

    Update page.tsx

--- projectVD/src/app/message/page.tsx
+++ projectVD/src/app/message/page.tsx
@@ -3,14 +3,6 @@
 import DefaultLayout from '@/components/Layouts/DefaultLayout';
 import React, { useState } from 'react';
 
-// TypeScript type for messages
-type MessageType = {
-  id: number;
-  text: string;
-  date: string;
-  sender: boolean;
-};
-
 function Message() {
 
   return (
