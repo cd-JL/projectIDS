2d32489397ea19738a98349425ea4dfca62cf314
Adwaith Ajith <adwaith.shiniajith@edu.sait.ca>
2024-11-01 05:51:03 -0600

    Messaging component updated

--- projectVD/src/app/message/page.tsx
+++ projectVD/src/app/message/page.tsx
@@ -3,12 +3,19 @@
 import DefaultLayout from '@/components/Layouts/DefaultLayout';
 import React, { useState } from 'react';
 import UsersList from './usersList';
+import MessageWindow from './messageWindow';
 
 function Message() {
 
   return (
     <DefaultLayout>
-        <UsersList/>
+        <div className="flex w-full h-screen bg-white text-white">
+            <div className="h-full w-1/3 bg-gray-800 p-4 flex flex-col">
+            
+            <UsersList/>
+            </div>
+            <MessageWindow/>
+        </div>
     </DefaultLayout>
   );
 }
--- projectVD/src/app/message/usersList.tsx
+++ projectVD/src/app/message/usersList.tsx
@@ -3,10 +3,8 @@ import React from 'react'
 function UsersList() {
   return (
     <div>
-        <div className="flex h-screen bg-white text-white">
         {/* Sidebar */}
-        <div className="w-1/3 bg-gray-800 p-4 flex flex-col">
-          <h1 className="text-2xl font-semibold mb-4">People</h1>
+        <h1 className="text-2xl font-semibold mb-4">People</h1>
           <div className="flex-1 overflow-y-auto">
             {/* Users list */}
               <div
@@ -14,12 +12,10 @@ function UsersList() {
               >
                 <img alt='Profile' src='image_source' className="rounded-full w-12 h-12"/>
                 <div className="flex flex-col">
-                  <p className="text-sm font-medium">Username</p>
+                  <p className="text-lg font-medium">Username</p>
                 </div>
               </div>
           </div>
-        </div>
-      </div>
     </div>
   )
 }
